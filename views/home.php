9673f3ad3c4a78b8aa00dbdc84d341e26b86c3ed
thezpark123 <axepau695@edu.linkoping.se>
2020-01-23 10:27:50 +0100

    try to fix

--- views/home.php
+++ views/home.php
@@ -5,13 +5,16 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Home</title>
 </head>
 <body>
 <div class="wrapper">
 
-    <?php require "../db.php";
-    $posts = getAll($_GET);
+    <?php
+    require "db.php";
+    $options = $_POST;
+    $posts = getAll($options);
+    var_dump($options);
 
     foreach ($posts as $post){
     ?>
