f29a4547f58686a0b31d75ea762a3a057c63a0ba
filvig505 <54351166+filvig505@users.noreply.github.com>
2020-01-30 10:49:39 +0100

    temp-delete, css update, uptadated non-functioning fileUploading

--- db.php
+++ db.php
@@ -67,3 +67,39 @@ function newPost($data){
         $success = runQuery($query);
         return $success;
 }
+
+function uploadFiles(){
+    if (isset($_POST['submit'])){
+        $file = $_FILES['img'];
+
+        $fileName = $_FILES['img']['name'];
+        $fileTmpName = $_FILES['img']['tmp_name'];
+        $fileSize = $_FILES['img']['size'];
+        $fileError = $_FILES['img']['type'];
+        $fileType = $_FILES['img']['Type'];
+
+        $fileExt = explode('.', $fileName);
+        $fileActualExt = strtolower(end($fileExt));
+
+        $allowed = array('jpg','jpeg','png','gif');
+        if (in_array($fileActualExt, $allowed)){
+            if ($fileError === 0){
+                if ($fileSize < 500000){
+                    $fileNameNew = uniqid('', true).".".$fileActualExt;
+                    $fileDestination = 'uploads/'.$fileNameNew;
+                    move_uploaded_file($fileTmpName, $fileDestination);
+                    header("location: /home");
+                }  else{
+                    echo "Your file is too big!"
+                    ;            }
+            }
+            else {
+                echo "There was an error uploading your file!";
+            }
+        }
+        else{
+            echo "You can not upload this type of file!";
+        }
+    }
+
+}
--- views/home.php
+++ views/home.php
@@ -4,12 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
 </head>
 <body>
-<h1>topmeme</h1>
-<div class="wrapper">
+
+<nav class="navbar navbar-light bg-black">
+    <img src="../Assets/topMeme-logo.jpg" width="150" height="28" class="d-inline-block align-top" alt="">
+    <a class="navbar-brand" id ="nav-text" href="newPost">Upload</a>
+    <a class="navbar-brand" id ="nav-text" href="uploads">temp delete</a>
+
+</nav>
+
+<div class="container">
 
     <?php require "db.php";
     $options = $_POST;
@@ -19,7 +28,8 @@
     ?>
 
         <div class="card">
-            <div class="card-text"><?= $post["caption"]?></div>
+            <img src="../Assets/topMeme.png"  class="card-img-top" alt="...">
+            <div class="card-title"><?= $post["caption"]?></div>
 
         </div>
 
