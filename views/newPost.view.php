1d991f8388ea3b8028ee186b10062f1f157717e0
thezpark123 <axepau695@edu.linkoping.se>
2020-02-06 10:16:21 +0100

    styling fix

--- views/newPost.view.php
+++ views/newPost.view.php
@@ -4,13 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="../stylesheets/main.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
+
+<nav class="navbar navbar-light bg-black">
+    <a href="home"><img src="../Assets/topMeme-logo.jpg" width="150" height="28" class="d-inline-block align-top" alt="site-logo"></a>
+    <a class="navbar-brand" id ="nav-text" href="newPost">Upload</a>
+    <a class="navbar-brand" id ="nav-text-2" href="delete">temp delete</a>
+</nav>
 
 <form action="upload" method="post" enctype="multipart/form-data">
     <div class="form-group">
