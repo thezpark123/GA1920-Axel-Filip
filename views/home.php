7b146a879f96b87a809ce8d88e5bcb577ecd1f27
filvig505 <54351166+filvig505@users.noreply.github.com>
2020-04-02 10:24:30 +0200

    update home.php

--- views/home.php
+++ views/home.php
@@ -2,24 +2,7 @@
 require "header.php"
 ?>
 <body>
-<div class="header">
-<nav class="navbar navbar-light bg-black">
-    <a href="home"><img src="../Assets/topMeme-logo.jpg" width="150" height="28" class="d-inline-block align-top" alt="site-logo"></a>
-    <a class="navbar-brand" id ="nav-text" href="newPost">Upload</a>
-    <form action="../includes/login.inc.php" method="post">
-        <input disabled type="text" name="mailuid" placeholder="username/e-mail...">
-        <input disabled type="password" name="pwd" placeholder="password">
-        <button type="submit" name="login-submit">Login</button>
-    </form>
 
-    <a href="signup">Signup</a>
-
-    <form action="../includes/logout.inc.php" method="post">
-        <button type="submit" name="logout-submit">Logout</button>
-    </form>
-</nav>
-
-</div>
 
 <div class="container">
 
