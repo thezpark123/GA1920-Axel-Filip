8a0f1196af2a6d1f0f4010b189951127c7918fc8
thezpark123 <axepau695@edu.linkoping.se>
2020-04-02 10:24:54 +0200

    Update header.php

--- views/header.php
+++ views/header.php
@@ -18,8 +18,8 @@
         <a href="home"><img src="../Assets/topMeme-logo.jpg" width="150" height="28" class="d-inline-block align-top" alt="site-logo"></a>
         <a class="navbar-brand" id ="nav-text" href="newPost">Upload</a>
         <form>
-            <input type="text" name="mailuid" placeholder="username/e-mail...">
-            <input type="password" name="pwd" placeholder="password">
+            <input disabled type="text" name="mailuid" placeholder="username/e-mail...">
+            <input disabled type="password" name="pwd" placeholder="password">
             <button type="submit" name="login-submit">Login</button>
         </form>
 
